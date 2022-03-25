375b0bf880f37e90ac8e409f248cce7ee60a82fa
dynepark_laptop <dynepark.dev@gmail.com>
2022-03-25 16:38:09 +0900

    feat: server test

--- server.js
+++ server.js
@@ -3,11 +3,23 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const PORT = process.env.PORT;
+const { getUpdatedList, Update } = require("./naver");
 
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get("/length", async (req, res) => {
+    const [$, $webtoonList] = await getUpdatedList();
+    const length = $webtoonList.length;
+    res.send(`Naver Updates Length : ${length}`);
+});
+
+app.get("/webtoons", async (req, res) => {
+    const webtoons = await Update();
+    res.json(webtoons);
 });
 
 app.listen(PORT, () => {

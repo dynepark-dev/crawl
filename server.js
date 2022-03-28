1bb8b6dc42b9bfcf8a411809478b6f5c5cd88a7c
dynepark-dev <dynepark.dev@gmail.com>
2022-03-29 05:30:11 +0900

    feat: added Dockerfile.dev & .dockerignore ref: README.md

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const { getUpdatedList, Update } = require("./naver");
 const cache = require("./routeCache");
 
 app.use(cors({ origin: "*" }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  res.send("Hello World!");
 });
 
-app.get("/length", cache(300), async (req, res) => {
-    const [$, $webtoonList] = await getUpdatedList();
-    const length = $webtoonList.length;
-    res.send(`Naver Updates Length : ${length}`);
+app.get("/length", cache(3000), async (req, res) => {
+  const [$, $webtoonList] = await getUpdatedList();
+  const length = $webtoonList.length;
+  res.send(`Naver Updates Length : ${length}`);
 });
 
-app.get("/webtoons", cache(300), async (req, res) => {
-    const webtoons = await Update();
-    res.json(webtoons);
+app.get("/webtoons", cache(3000), async (req, res) => {
+  const webtoons = await Update();
+  res.json(webtoons);
 });
 
 app.listen(PORT, () => {
-    console.log(`port: ${PORT}`);
+  console.log(`port: ${PORT}`);
 });
